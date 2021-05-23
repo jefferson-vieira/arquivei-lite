9b2dfbc6f0e8d7c854e474908aa1b9b08e8f9d7b
Jefferson Vieira da Silva <jefferson98.vieira@gmail.com>
2021-05-23 17:52:10 -0300

    fix: configurar o jest para resolver imports de css

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,5 @@ module.exports = {
   coverageProvider: 'v8',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testPathIgnorePatterns: ['/node_modules/', '<rootDir>/.next/'],
+  moduleNameMapper: { '\\.(s?css|less)$': 'identity-obj-proxy' },
 };
